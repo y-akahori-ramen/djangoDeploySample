0a672befe202158323b489c4c6ace9c40efc22ee
Yuki Akahori <y.akahori.ramen@gmail.com>
2020-09-07 23:38:05 +0900

    テンプレート環境追加

---
+++ infra/python/data/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# 不要になる静的コンテンツを残したくないためclearオプションで毎回コピー前に削除する
+python /home/app/sampleapp/manage.py collectstatic --noinput --clear
+
+# マイグレーション
+# ※djangoプロジェクト内のアプリが増えた場合はその分 makemigrations アプリ名 が必要
+python /home/app/sampleapp/manage.py makemigrations
+python /home/app/sampleapp/manage.py migrate
+
+# サーバー起動
+# python /home/app/sampleapp/manage.py runserver 0.0.0.0:8000
+uwsgi --ini /home/data/django_uwsgi.ini
